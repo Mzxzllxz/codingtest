17be99e6ead69bab88330b37e69815357f46566e
Mzxzllxz <xzllxz456@gmail.com>
2024-08-19 14:56:06 +0900

    20240819study

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        bak10813 a = new bak10813();
+//        a.ballChange();
+        Back28278 a = new Back28278();
+        a.stack2();
+
+    }
+}
